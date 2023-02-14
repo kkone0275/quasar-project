bccb4ec3b301cbc0adb9c078d8803804adc0f81c
klion9280 <lone8877@gmail.com>
2023-02-14 13:15:11 +0800

    02.14恢復

--- src/pages/back/ActivityUp.vue
+++ src/pages/back/ActivityUp.vue
@@ -1,27 +1,42 @@
 <template>
   <h5 class="text-center">活動上架</h5>
   <q-btn class="add" style="background: #F3A308; color: white" @click="openAdd(-1)" label="新增揪團" />
+  <table class="box" style="width: 60%; " border="1">
+          <thead>
+            <tr align="left">
+              <th>圖片</th>
+              <th>名稱</th>
+              <th>管理</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(product, idx) in products" :key="product._id">
+              <td align="center">
+                <img :src="product.image" :aspect-ratio="1" :width="100" :height="100"
+                style="object-fit: cover; margin: auto;">
+              </td>
+              <td>{{ product.name }}</td>
+              <td align="center">
+                <q-btn color="primary" icon="edit" @click="openAdd(idx)" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
   <div class="q-pa-md">
     <div class="row justify-center">
       <div class="col-12 col-md-1">
         <q-dialog v-model="form.dialog" persistent>
-          <q-card style="min-width: 800px ">
+          <q-card style="max-width: 800px ">
             <div class="text-h6" align="center">{{ form._id.length > 0 ? '編輯揪團' : '新增揪團' }}</div>
             <q-form @submit="submit">
               <div class="flex row justify-between" style="padding: 16px 50px 16px 50px;">
               <q-input class="col-12" style="padding:10px ;" filled v-model="form.name" label="活動名稱" lazy-rules :rules="[rules.required]"/>
               <q-input class="col-12" style="padding:10px ;" filled v-model="form.price" label="活動價格" lazy-rules :rules="[rules.required,rules.price]"/>
-              <q-input class="col-12" style="padding: 10px;"
-                v-model="form.description"
-                filled clearable
-                type="textarea"
-                color="red-12"
-                label="活動說明"
-                hint="輸入內文"
-                :shadow-text="textareaShadowText" @keydown="processTextareaFill" @focus="processTextareaFill"
-                :rules="[rules.required]"
-              />
-              <q-file class="col-12 " filled bottom-slots v-model="form.image" label="活動圖片" counter>
+              <q-input class="col-12" style="padding: 10px;" filled v-model="form.description" label="商品說明"
+              clearable type="textarea" @keydown="processTextareaFill"
+              @focus="processTextareaFill"
+              :rules="[rules.required]"/>
+              <q-file class="col-12" filled bottom-slots v-model="form.image" label="活動圖片" counter>
                 <template v-slot:prepend>
                   <q-icon name="cloud_upload" @click.stop.prevent />
                 </template>
@@ -38,11 +53,9 @@
 
               <q-checkbox class="col-8" style="margin-bottom: 1.2rem;" v-model="form.sell" label="上架" />
 
-              <!-- <q-btn class="col-6" label="Send" type="submit" :loading="loading" color="primary" /> -->
+              <q-btn class="col-6" style="padding: 10px;" flat label="Cancel" color="primary" :disabled="form.loading" v-close-popup />
 
-              <q-btn class="col-6" style="padding: 10px;" flat label="Cancel" color="primary" v-close-popup />
-
-              <q-btn class="col-6" flat label="Add Send" type="submit" :loading="loading" color="primary" />
+              <q-btn class="col-6" flat label="Add Send" type="submit" :disabled="form.loading" color="primary" />
               </div>
             </q-form>
           </q-card>
@@ -57,64 +70,7 @@
   position: absolute;
   right: 16%;
 }
-</style>
 
-<script setup>
-// import { apiAuth } from '../../boot/axios.js'
-import { reactive } from 'vue'
-
-const rules = {
-  required (value) {
-    return !!value || '欄位必填'
-  },
-  price (value) {
-    return value >= 0 || '價格錯誤'
-  }
-}
-
-const categories = ['台北市', '新北市', '新竹市', '台中市', '雲林縣', '台中市']
-
-const form = reactive({
-  _id: '',
-  name: '',
-  price: 0,
-  description: '',
-  image: undefined,
-  sell: false,
-  category: '',
-  valid: false,
-  loading: false,
-  dialog: false
-})
-
-const openAdd = (idx) => {
-  if (idx === -1) {
-    form._id = ''
-    form.name = ''
-    form.price = 0
-    form.description = ''
-    form.image = undefined
-    form.sell = false
-    form.category = ''
-    form.valid = false
-    form.loading = false
-  } else {
-    // form._id = products[idx]._id
-    // form.name = products[idx].name
-    // form.price = products[idx].price
-    // form.description = products[idx].description
-    // form.image = undefined
-    // form.sell = products[idx].sell
-    // form.category = products[idx].category
-    // form.valid = false
-    // form.loading = false
-    // form.idx = idx
-  }
-  form.dialog = true
-}
-</script>
-
-<style>
 .q-card{
   height: 600px;
 }
@@ -128,4 +84,123 @@ const openAdd = (idx) => {
   margin-bottom: 20px;
 }
 
+.q-select{
+  margin-top: 25px
+}
+
+.box{
+  position: absolute;
+  top: 30%;
+  left: 25%;
+}
 </style>
+
+<script setup>
+import { apiAuth } from '../../boot/axios.js'
+import { reactive } from 'vue'
+import Swal from 'sweetalert2'
+
+const categories = ['台北市', '新北市', '新竹市', '台中市', '雲林縣', '台中市']
+const rules = {
+  required (value) {
+    return !!value || '欄位必填'
+  },
+  price (value) {
+    return value >= 0 || '價格錯誤'
+  }
+}
+
+const products = reactive([])
+const form = reactive({
+  _id: '',
+  name: '',
+  price: 0,
+  description: '',
+  image: undefined,
+  sell: false,
+  category: '',
+  valid: false,
+  loading: false,
+  dialog: false,
+  idx: -1
+})
+
+const openAdd = (idx) => {
+  if (idx === -1) {
+    form._id = ''
+    form.name = ''
+    form.price = 0
+    form.description = ''
+    form.image = undefined
+    form.sell = false
+    form.category = ''
+    form.valid = false
+    form.loading = false
+    form.idx = -1
+  } else {
+    form._id = products[idx]._id
+    form.name = products[idx].name
+    form.price = products[idx].price
+    form.description = products[idx].description
+    form.image = undefined
+    form.sell = products[idx].sell
+    form.category = products[idx].category
+    form.valid = false
+    form.loading = false
+    form.idx = idx
+  }
+  form.dialog = true
+}
+
+const submit = async () => {
+  form.loading = true
+  const fd = new FormData()
+  fd.append('name', form.name)
+  fd.append('price', form.price)
+  fd.append('description', form.description)
+  fd.append('sell', form.sell)
+  fd.append('image', form.image)
+  fd.append('category', form.category)
+
+  try {
+    if (form._id.length === 0) {
+      const { data } = await apiAuth.post('/products', fd)
+      products.push(data.result)
+      Swal.fire({
+        icon: 'success',
+        title: '成功',
+        text: '新增成功'
+      })
+    } else {
+      const { data } = await apiAuth.patch('/products/' + form._id, fd)
+      products[form.idx] = data.result
+      Swal.fire({
+        icon: 'success',
+        title: '成功',
+        text: '編輯成功'
+      })
+    }
+    form.dialog = false
+  } catch (error) {
+    Swal.fire({
+      icon: 'error',
+      title: '失敗',
+      text: error?.response?.data?.message || '發生錯誤'
+    })
+  }
+  form.loading = false
+}
+
+(async () => {
+  try {
+    const { data } = await apiAuth.get('/products/all')
+    products.push(...data.result)
+  } catch (error) {
+    Swal.fire({
+      icon: 'error',
+      title: '失敗',
+      text: error?.response?.data?.message || '發生錯誤'
+    })
+  }
+})()
+</script>
